3fe904fcb67d76b9188689c4bea989976b3f1fd7
Prisca Marie <prisca.ros@onja.org>
2021-08-10 19:24:26 +0300

    refactor(HomePage): refactored navHeaderLinks content in the commonPageComponent.
    refs: #6n3v5d -C32

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -1,136 +1,154 @@
-import styled from "styled-components"
-import { CommonPageContainer } from "../components/CommonPageContainer/CommonPageContainer";
-import LearnMoreBanner from "../components/LearnMoreBanner/LearnMoreBanner";
-import ImageBanner from '../stories/assets/banner.png';
-import { CategoryList } from "../components/CategoryList/CategoryList";
-import {Offer} from "../components/Offer/Offer";
+import styled from 'styled-components'
+import { CommonPageContainer } from '../components/CommonPageContainer/CommonPageContainer'
+import LearnMoreBanner from '../components/LearnMoreBanner/LearnMoreBanner'
+import ImageBanner from '../stories/assets/banner.png'
+import { CategoryList } from '../components/CategoryList/CategoryList'
+import { Offer } from '../components/Offer/Offer'
 import Image from '../stories/assets/offer.svg'
 import SellerPrevImage from '../stories/assets/seller-prev-img.png'
-import { mediaQueriesPx } from "../mediaQueries";
-import { CallToAction } from "../components/CallToAction/CallToAction";
+import { mediaQueriesPx } from '../mediaQueries'
+import { CallToAction } from '../components/CallToAction/CallToAction'
 const categories = [
   { id: '1', title: 'Food' },
   { id: '2', title: 'Handmade' },
   { id: '3', title: 'Spices' },
   { id: '4', title: 'Collectibles' },
-  {id: '5', title: 'Everything'},
+  { id: '5', title: 'Everything' },
 ]
 
 const offers = [
   {
-  imageForOffer: Image,
-  imageDescription: 'Image of offer',
-  offerName: 'Cocoa nuts',
-  currency: 'Euro',
-  amount: 25.0,
-  unit: 'kilo',
-  star: 3,
-  ratingDescription: 'star',
-  amountOfProduct: 27,
-  offerDescription:
-    'This is a very detailed description that is absolutely necessary for being attractive to customers...',
-  isLearnEnabled: true,
-  isFavourited: false,
-  detailButtonText: 'See details',
-  favoriteButtonText: 'Add to favourite',
-  profile: SellerPrevImage,
-  name: {
-    firstName: 'Eliud',
-    lastName: 'Kipchoge',
+    imageForOffer: Image,
+    imageDescription: 'Image of offer',
+    offerName: 'Cocoa nuts',
+    currency: 'Euro',
+    amount: 25.0,
+    unit: 'kilo',
+    star: 3,
+    ratingDescription: 'star',
+    amountOfProduct: 27,
+    offerDescription:
+      'This is a very detailed description that is absolutely necessary for being attractive to customers...',
+    isLearnEnabled: true,
+    isFavourited: false,
+    detailButtonText: 'See details',
+    favoriteButtonText: 'Add to favourite',
+    profile: SellerPrevImage,
+    name: {
+      firstName: 'Eliud',
+      lastName: 'Kipchoge',
+    },
+    location: {
+      country: 'Madagascar',
+      city: 'Mahanoro',
+    },
+    id: 1,
   },
-  location: {
-    country: 'Madagascar',
-    city: 'Mahanoro',
-  }, 
-  id: 1,
-},
-{imageForOffer: Image,
-  imageDescription: 'Image of offer',
-  offerName: 'Cocoa nuts',
-  currency: 'Euro',
-  amount: 25.0,
-  unit: 'kilo',
-  star: 3,
-  ratingDescription: 'star',
-  amountOfProduct: 27,
-  offerDescription:
-    'This is a very detailed description that is absolutely necessary for being attractive to customers...',
-  isLearnEnabled: true,
-  isFavourited: false,
-  detailButtonText: 'See details',
-  favoriteButtonText: 'Add to favourite',
-  profile: SellerPrevImage,
-  name: {
-    firstName: 'Eliud',
-    lastName: 'Kipchoge',
+  {
+    imageForOffer: Image,
+    imageDescription: 'Image of offer',
+    offerName: 'Cocoa nuts',
+    currency: 'Euro',
+    amount: 25.0,
+    unit: 'kilo',
+    star: 3,
+    ratingDescription: 'star',
+    amountOfProduct: 27,
+    offerDescription:
+      'This is a very detailed description that is absolutely necessary for being attractive to customers...',
+    isLearnEnabled: true,
+    isFavourited: false,
+    detailButtonText: 'See details',
+    favoriteButtonText: 'Add to favourite',
+    profile: SellerPrevImage,
+    name: {
+      firstName: 'Eliud',
+      lastName: 'Kipchoge',
+    },
+    location: {
+      country: 'Madagascar',
+      city: 'Mahanoro',
+    },
+    id: 2,
   },
-  location: {
-    country: 'Madagascar',
-    city: 'Mahanoro',
-  }, 
-  id: 2,
-},{
-  imageForOffer: Image,
-  imageDescription: 'Image of offer',
-  offerName: 'Cocoa nuts',
-  currency: 'Euro',
-  amount: 25.0,
-  unit: 'kilo',
-  star: 3,
-  ratingDescription: 'star',
-  amountOfProduct: 27,
-  offerDescription:
-    'This is a very detailed description that is absolutely necessary for being attractive to customers...',
-  isLearnEnabled: true,
-  isFavourited: false,
-  detailButtonText: 'See details',
-  favoriteButtonText: 'Add to favourite',
-  profile: SellerPrevImage,
-  name: {
-    firstName: 'Eliud',
-    lastName: 'Kipchoge',
+  {
+    imageForOffer: Image,
+    imageDescription: 'Image of offer',
+    offerName: 'Cocoa nuts',
+    currency: 'Euro',
+    amount: 25.0,
+    unit: 'kilo',
+    star: 3,
+    ratingDescription: 'star',
+    amountOfProduct: 27,
+    offerDescription:
+      'This is a very detailed description that is absolutely necessary for being attractive to customers...',
+    isLearnEnabled: true,
+    isFavourited: false,
+    detailButtonText: 'See details',
+    favoriteButtonText: 'Add to favourite',
+    profile: SellerPrevImage,
+    name: {
+      firstName: 'Eliud',
+      lastName: 'Kipchoge',
+    },
+    location: {
+      country: 'Madagascar',
+      city: 'Mahanoro',
+    },
+    id: 3,
   },
-  location: {
-    country: 'Madagascar',
-    city: 'Mahanoro',
-  }, 
-  id: 3,
-}]
+]
 
 export default function HomePage() {
   return (
-        <CommonPageContainer>
-            <Main>
-                <LearnMoreBanner bannerHeading="Madagascars peer-to-peer e-commerce platform" bannerDescription = "Purchase high-quality products made by the people that sell them. By cutting out middlemen, you know exactly where your purchase is from and how it was made." bannerImage={ImageBanner} />
-                <CategoryLabel>Top Categories</CategoryLabel>
-                <CategoryList categories={categories} primary={true} selectCategory={() => {}}/>
-                <Separator/>
-                <OfferList>
-                  {offers.map(offer => {
-                    return <Offer key={offer.id} 
-                      imageForOffer ={Image}
-                      imageDescription = {offer.imageDescription}
-                      imageForRating=''
-                      offerName= {offer.offerName}
-                      currency= {offer.currency}
-                      amount={offer.amount}
-                      unit= {offer.unit}
-                      star= {offer.star}
-                      ratingDescription = {offer.ratingDescription}
-                      amountOfProduct={offer.amountOfProduct}
-                      offerDescription={offer.offerDescription}
-                      isLearnEnabled={offer.isLearnEnabled}
-                      isFavourited= {offer.isFavourited}
-                      detailButtonText= {offer.detailButtonText}
-                      favoriteButtonText= {offer.favoriteButtonText}
-                      profile= {offer.profile}
-                      name={offer.name}
-                      location={offer.location}/>
-                    })}
-                </OfferList>
-                <CallToAction title="Discover amazing products and profit from a truly fair market:" subTitle="This is a subtitle that is very informative"/>
-            </Main>
-        </CommonPageContainer>
+    <CommonPageContainer>
+      <Main>
+        <LearnMoreBanner
+          bannerHeading='Madagascars peer-to-peer e-commerce platform'
+          bannerDescription='Purchase high-quality products made by the people that sell them. By cutting out middlemen, you know exactly where your purchase is from and how it was made.'
+          bannerImage={ImageBanner}
+        />
+        <CategoryLabel>Top Categories</CategoryLabel>
+        <CategoryList
+          categories={categories}
+          primary={true}
+          selectCategory={() => {}}
+        />
+        <Separator />
+        <OfferList>
+          {offers.map((offer) => {
+            return (
+              <Offer
+                key={offer.id}
+                imageForOffer={Image}
+                imageDescription={offer.imageDescription}
+                imageForRating=''
+                offerName={offer.offerName}
+                currency={offer.currency}
+                amount={offer.amount}
+                unit={offer.unit}
+                star={offer.star}
+                ratingDescription={offer.ratingDescription}
+                amountOfProduct={offer.amountOfProduct}
+                offerDescription={offer.offerDescription}
+                isLearnEnabled={offer.isLearnEnabled}
+                isFavourited={offer.isFavourited}
+                detailButtonText={offer.detailButtonText}
+                favoriteButtonText={offer.favoriteButtonText}
+                profile={offer.profile}
+                name={offer.name}
+                location={offer.location}
+              />
+            )
+          })}
+        </OfferList>
+        <CallToAction
+          title='Discover amazing products and profit from a truly fair market:'
+          subTitle='This is a subtitle that is very informative'
+        />
+      </Main>
+    </CommonPageContainer>
   )
 }
 
@@ -139,7 +157,7 @@ const Main = styled.main`
   align-items: center;
   justify-content: center;
   padding-bottom: 318px;
-  
+
   & > div:last-of-type {
     position: absolute;
     -webkit-transform: translateX(-50%);
@@ -160,20 +178,20 @@ const Main = styled.main`
       margin-top: 144px;
     `}
   }
-`;
+`
 const CategoryLabel = styled.h3`
-    font-family: Futura Std;
-    font-style: normal;
-    font-weight: normal;
-    font-size: 30px;
-    line-height: 36px;
-    color: #041D42;
-    margin-top: 43px;
-    margin-bottom: 13px;
-    ${mediaQueriesPx('lg', null)`
+  font-family: Futura Std;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 30px;
+  line-height: 36px;
+  color: #041d42;
+  margin-top: 43px;
+  margin-bottom: 13px;
+  ${mediaQueriesPx('lg', null)`
     margin-top: 124px;
   `}
-`;
+`
 const OfferList = styled.div`
   margin-top: 39px;
   display: flex;
@@ -182,7 +200,7 @@ const OfferList = styled.div`
   ${mediaQueriesPx('lg', null)`
     margin-top: 30px;
   `}
-`;
+`
 const Separator = styled.hr`
   display: none;
   margin: 0;
@@ -191,4 +209,4 @@ const Separator = styled.hr`
     border-top: 1px solid #041D42;
     display: block;
   `}
-`;
+`
--- src/components/RoleSelectOnboard/RoleSelectOnboard.tsx
+++ src/components/RoleSelectOnboard/RoleSelectOnboard.tsx
@@ -1,65 +1,72 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import {
-	TopContainerStyles,
-	MainContainer,
-	RoleOptionContainer,
-	Container,
-	AccountContainer,
-	HeaderContainer,
-	ImageContainer,
-	JoinUsHeader,
-	FooterContainer
+  TopContainerStyles,
+  MainContainer,
+  RoleOptionContainer,
+  Container,
+  AccountContainer,
+  HeaderContainer,
+  ImageContainer,
+  JoinUsHeader,
+  FooterContainer,
 } from './RoleSelectOnboardStyles'
 
 import { DescriptionOffer } from '../DescriptionOffer/descriptionOffer'
 import { PageFooter, Props } from '../PageFooter/PageFooter'
 import { Header } from '../Header'
-import { loggedIn } from '../HeaderNavLink/HeaderNavLink'
+import { loggedOut } from '../HeaderNavLink/HeaderNavLink'
 import { RoleSelectOption } from '../RoleSelectOption/RoleSelectOption'
 import { Login } from '../Login/Login'
 import { LeftSide } from '../LeftSide/LeftSide'
 
 export const RoleSelectOnboard: React.FC<Props> = ({ footerLinks }) => {
-	const roleOptions = [{label: "Buyer", id: '1'}, {label: "Seller", id: '2'}];
-	const [role, setRole] = useState({label: '', id: ''})
-	
-	return (
-		<Container>
-			<TopContainerStyles>
-				<HeaderContainer>
-					<Header item={loggedIn} />
-				</HeaderContainer>
-				<MainContainer>
-					<ImageContainer>
-						<LeftSide backgroundImage='Baobab' />
-					</ImageContainer>
-					<RoleOptionContainer>
-						{role.label 
-						?
-						<p>Form field of account registration will be here ....</p>
-					:
-					<div>
-							<JoinUsHeader>Join us</JoinUsHeader>
-							<DescriptionOffer
-								text={"To begin this story, tell us what kind of account you'd be opening "}
-							/>
-							{roleOptions.map(role => (
-							<RoleSelectOption key={role.id} setRole={setRole}
-								role={role}
-								label={role.label}
-								text='Own or belong to a company, this is for you.'
-							/>
-							))}
-							<AccountContainer>
-								<Login isSignedUp={true} href='./' />
-							</AccountContainer>
-						</div> }
-					</RoleOptionContainer>
-				</MainContainer>
-			</TopContainerStyles>
-			<FooterContainer>
-				<PageFooter footerLinks={footerLinks} />
-			</FooterContainer>
-		</Container>
-	)
+  const roleOptions = [
+    { label: 'Buyer', id: '1' },
+    { label: 'Seller', id: '2' },
+  ]
+  const [role, setRole] = useState({ label: '', id: '' })
+
+  return (
+    <Container>
+      <TopContainerStyles>
+        <HeaderContainer>
+          <Header item={loggedOut} />
+        </HeaderContainer>
+        <MainContainer>
+          <ImageContainer>
+            <LeftSide backgroundImage='Baobab' />
+          </ImageContainer>
+          <RoleOptionContainer>
+            {role.label ? (
+              <p>Form field of account registration will be here ....</p>
+            ) : (
+              <div>
+                <JoinUsHeader>Join us</JoinUsHeader>
+                <DescriptionOffer
+                  text={
+                    "To begin this story, tell us what kind of account you'd be opening "
+                  }
+                />
+                {roleOptions.map((role) => (
+                  <RoleSelectOption
+                    key={role.id}
+                    setRole={setRole}
+                    role={role}
+                    label={role.label}
+                    text='Own or belong to a company, this is for you.'
+                  />
+                ))}
+                <AccountContainer>
+                  <Login isSignedUp={true} href='./' />
+                </AccountContainer>
+              </div>
+            )}
+          </RoleOptionContainer>
+        </MainContainer>
+      </TopContainerStyles>
+      <FooterContainer>
+        <PageFooter footerLinks={footerLinks} />
+      </FooterContainer>
+    </Container>
+  )
 }
