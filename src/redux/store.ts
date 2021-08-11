f6b4d2f592390f5a575be08a96c923ab0b46f7e3
Jerome Emilien <jerome.emi@onja.org>
2021-08-11 11:09:12 +0300

    feat(Home Page): Fetch categories and display on Home Page
    _ Fixes margin with Home header
    
    _ Updates graphQlHelper and store file
    
    _ Creates categoriesSlice for fetching categories
    
    _ Updates Home Page for rendering categories fetch and desplaying the categories
    
    _ Adds loading indicator for categories list
    
    _ Removes all of comments on categories slice
    
    _ Uses status state to display loading indicator
    
    _ Solves conflicts
    
    Refs: T2 #4deyhf

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,26 +1,23 @@
+import { combineReducers } from "@reduxjs/toolkit";
 import adsReducer from "./slices/adsSlice";
-import { combineReducers } from '@reduxjs/toolkit';
-import { createBrowserHistory } from 'history'
-import { routerMiddleware, connectRouter } from 'connected-react-router'
-import { applyMiddleware, compose, createStore } from 'redux'
-import thunk from "redux-thunk"
+import categoriesReducer from "./slices/categoriesSlice";
+import { createBrowserHistory } from "history";
+import { connectRouter, routerMiddleware } from "connected-react-router";
+import { configureStore } from "@reduxjs/toolkit";
 
-export const history = createBrowserHistory()
+export const history = createBrowserHistory();
 
-const createRootReducer = (history: any) => combineReducers({
-  router: connectRouter(history),
+const rootReducer = combineReducers({
+  router: connectRouter(history) as any,
   ads: adsReducer,
-})
+  categories: categoriesReducer
+});
 
-export const store = (function configureStore() {
-  const store = createStore(
-    createRootReducer(history),
-    compose(applyMiddleware(routerMiddleware(history), thunk)),
-  )
-  return store
-})()
+export type RootState = ReturnType<typeof rootReducer>;
+export const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(routerMiddleware(history)),
+});
 
-// Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>;
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
